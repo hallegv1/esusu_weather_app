defafd0400f07e66349c1061439957cf785d8efb
hallegv1 <hallevogelpohl99@gmail.com>
2024-11-20 17:07:25 -0800

    Uncomment accidental push of testing toast

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -24,20 +24,20 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   ) async {
     emit(WeatherStateLoading());
 
-    // try {
-    //   final currentLocation = await locationRepository.getCurrentLocation();
+    try {
+      final currentLocation = await locationRepository.getCurrentLocation();
 
-    //   double latitude = event.latitude ?? currentLocation.latitude;
-    //   double longitude = event.longitude ?? currentLocation.longitude;
+      double latitude = event.latitude ?? currentLocation.latitude;
+      double longitude = event.longitude ?? currentLocation.longitude;
 
-    //   final weather = await weatherRepository.fetchWeather(
-    //     latitude: latitude,
-    //     longitude: longitude,
-    //   );
+      final weather = await weatherRepository.fetchWeather(
+        latitude: latitude,
+        longitude: longitude,
+      );
 
-    //   emit(WeatherStateSuccess(weather));
-    // } catch (_) {
-    emit(WeatherStateFailure());
-    // }
+      emit(WeatherStateSuccess(weather));
+    } catch (_) {
+      emit(WeatherStateFailure());
+    }
   }
 }
